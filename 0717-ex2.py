399e81ff9c3eeed40c00c182cd9219f71d51751e
anita602088 <xinronglee60326@gmail.com>
2018-07-17 16:13:18 +0800

    Test

---
+++ 0717-ex2.py
@@ -0,0 +1,10 @@
+import csv
+
+with open("AQI.csv", "r", encoding='utf-8-sig') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+
+list:[None,200]
+for line[1] in f :
+    if list[2]< list[1]:
